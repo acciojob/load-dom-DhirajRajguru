bae160feb4e2fc0d4550c6a30d11df6f05aaea3c
DhirajRajguru <110520184+DhirajRajguru@users.noreply.github.com>
2023-10-22 23:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 ducument.addEventListener("DOMContentLoaded",function(){
-	let bod = document.getElementByTagName('body')[0];
-	bod.textContent="DOM load success";
+	let body = document.getElementByTagName('body')[0];
+	body.textContent="DOM load success";
 	
 });
