f94e0af031adee3f1c51f6bc86ab0627553f8367
DhirajRajguru <110520184+DhirajRajguru@users.noreply.github.com>
2023-10-23 19:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function onDOMLoad() {
-            // Create a new text node with the message
-            var textNode = document.createTextNode("DOM load success");
-
-            // Append the text node to the body element
-            document.body.appendChild(textNode);
-        }
-
-        // Add an event listener to run the function when the DOM is loaded
-        document.addEventListener("DOMContentLoaded", onDOMLoad);
+//your JS code here. If required.
+  document.addEventListener("DOMContentLoaded", function() {
+        document.body.textContent = "DOM load success";
+ });
